c8d328b43a0b8fa8231bbfb2f5cd89d85fe5fe2f
Alohaman2006 <alohaman2006@gmail.com>
2025-10-20 17:00:51 +0200

    giga autism

---
+++ threejs.js
@@ -0,0 +1,83 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import getStarfield from "/getStarfield.js";
+import { getFresnelMat } from "/getFresnelMat.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const fugGroup = new THREE.Group();
+scene.add(fugGroup);
+
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 12);
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("images/fugeria.jpg"),
+});
+const fugeria = new THREE.Mesh(geometry, material);
+fugGroup.add(fugeria);
+
+//const lightsMat = new THREE.MeshBasicMaterial({
+  //map: loader.load("images/fugeria_night.jpg"),
+  //blending: THREE.AdditiveBlending,
+//});
+//const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+//fugGroup.add(lightsMesh);
+
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("images/clouds.jpg"),
+  alphaMap: loader.load("images/cloudmaptrans.jpg"),
+  transparent: true,
+  depthWrite: false,
+  opacity: 0.2,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.003);
+fugGroup.add(cloudsMesh);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+fugGroup.add(glowMesh);
+
+const stars = getStarfield({ numStars: 20000 });
+scene.add(stars);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambient)
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  fugeria.rotation.y += 0.002;
+  //lightsMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.0023;
+  glowMesh.rotation.y += 0.002;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
